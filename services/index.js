1fa8c622837511554df69cdb30430acee7e33d2c
Andreiferu <98920687+Andreiferu@users.noreply.github.com>
2025-08-11 23:47:32 +0300

    Create index.js

---
+++ services/index.js
@@ -0,0 +1,4 @@
+export { analyticsCalculator } from './calculations/analytics';
+export { localStorageService } from './storage/localStorage';
+export { subscriptionsService } from './api/subscriptions';
+export { emailsService } from './api/emails';
