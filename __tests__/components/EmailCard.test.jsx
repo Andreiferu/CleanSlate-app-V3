902d94ea796266d54b0cef00df3a8fff4e430684
Andreiferu <98920687+Andreiferu@users.noreply.github.com>
2025-08-12 22:42:21 +0300

    Update EmailCard.test.jsx

--- __tests__/components/EmailCard.test.jsx
+++ __tests__/components/EmailCard.test.jsx
@@ -1,3 +1,4 @@
+import { ChevronUp, ChevronDown } from 'lucide-react';
 import React from 'react';
 import { render, fireEvent, waitFor, screen } from '@testing-library/react';
 import '@testing-library/jest-dom';
