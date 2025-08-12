a8fbd37ec59cfa55cda44f90e05352c4d3a27e36
Andreiferu <98920687+Andreiferu@users.noreply.github.com>
2025-08-12 23:34:44 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -2,7 +2,6 @@ const nextConfig = {
   reactStrictMode: true,
   swcMinify: true,
   eslint: { ignoreDuringBuilds: true },
-  experimental: { appDir: false },
   images: {
     formats: ['image/webp', 'image/avif'],
     deviceSizes: [640, 750, 828, 1080, 1200, 1920],
