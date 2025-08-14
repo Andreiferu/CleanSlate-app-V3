0a3af12d91b974c85c74ca85a0bfaa6c9e798b78
Andreiferu <98920687+Andreiferu@users.noreply.github.com>
2025-08-14 22:41:44 +0300

    Create index.js

---
+++ services/nordigen/index.js
@@ -0,0 +1,3 @@
+// services/nordigen/index.js
+export { getNordigenAuth, nordigenRequest } from './auth.js';
+export { getSubscriptionDetector, detectSubscriptions } from './subscriptionDetector.js';
